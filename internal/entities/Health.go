3116bd89bafb6bb983217a3e1a8b0c099d1f3bd8
soloma <112769690+iamsoloma@users.noreply.github.com>
2025-11-18 02:17:31 +0500

    Revert "Izamaes`s structure"

--- internal/entities/Health.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-type Health struct {
-	Status       string `json:"Status"`
-	CurrentTime  string `json:"CurrentTime"`
-	Uptime       string `json:"Uptime"`
-	Version      string `json:"BackendVersion"`
-	DatabaseInfo string `json:"DBName"`
-}
