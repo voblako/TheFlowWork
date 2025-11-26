33080c49ca41366c1f373fc1400d42ac98fcf990
soloma <EgorSolomahin1@yandex.ru>
2025-11-26 17:52:27 +0500

    delete return

--- internal/api/health.go
+++ internal/api/health.go
@@ -30,5 +30,4 @@ func (s *Server) Health(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonResp)
-	return
 }
